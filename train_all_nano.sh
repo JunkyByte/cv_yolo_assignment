ce7021004c52aec16466bcf73bdbf0b1231f7915
JunkyByte <adriano.donninelli@hotmail.it>
2025-04-16 10:19:16 +0200

    minor

---
+++ train_all_nano.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+YAML_FILE=models.yaml
+MODELS=$(yq '.nano[]' "$YAML_FILE")
+
+for MODEL in $MODELS; do
+  echo "Training with model: $MODEL"
+  python train.py \
+    --model_name "$MODEL" \
+    --models_yaml models.yaml \
+    --data_yaml /home/adryw/dataset/coco_cv/coco/coco.yaml \
+    --epochs 30 \
+    --img_size 640
+done
