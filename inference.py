7de38ceb409692e5b3d553dc066f514b49a9d02e
JunkyByte <adriano.donninelli@hotmail.it>
2025-04-26 12:18:03 +0200

    final

--- inference.py
+++ inference.py
@@ -6,7 +6,7 @@ from ultralytics import YOLO
 @click.option('--image-path', required=True, help='Path to the image file')
 def run_inference(model_path, image_path):
     model = YOLO(model_path)
-    results = model(image_path)
+    results = model.predict(image_path, imgsz=640, conf=0.2)
     results[0].show()
 
 if __name__ == '__main__':
